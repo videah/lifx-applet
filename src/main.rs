4f53e5ab4122dc4c29937544abf76bcaedd04c93
videah <videah@selfish.systems>
2019-01-31 02:23:42 +0000

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate systray;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create applet window")
+    }
+    app.set_icon_from_file(&"/usr/share/gxkb/flags/ua.png".to_string()).ok();
+    app.add_menu_item(&"Toggle Light".to_string(), |_| {
+        println!("Clicked");
+    }).ok();
+    app.wait_for_message();
 }
